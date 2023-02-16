fd314762d5d9524ae4fa8f393f8e357d7fce120f
EstAlvB <estebanalvarado@MacBook-Air-de-Esteban.local>
2023-02-15 21:23:57 -0500

    netbeans project added

---
+++ Project1-SimpleShell/src/main/java/SimpleShell.java
@@ -0,0 +1,15 @@
+/**
+ * @author gabriel-lara
+ * @author alexander-pastor
+ * @author esteban-alvarado
+ */
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SimpleShell {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
